68750812acbd1be7f1b1dc1b70b2aae6016ea91b
Justin Cormack <justin@specialbusservice.com>
2010-11-23 14:21:49 +0000

    prototypal version of vbuf
    still needs init, hmm

--- pngback.js
+++ pngback.js
@@ -9,6 +9,7 @@
 // merge all the functions into one big one stops the stupid lack of encapsulation shit, and we can store all the state in the function until we fire event
 
 // cant do what we wanted and cleanup to not use vbuf now, as functions not clean any more, too entangled in match. will redo
+// although actually I still think that for this case, getting all of chunk data is easier, lets not stream partial chunks
 
 // dont make objects reusble - ie no init methods. Make a new one for a new operation. Create prototypes in right state
 
@@ -23,62 +24,63 @@ events.EventEmitter.prototype.on2 = function(ev, f, scope) {
 
 // data object for node buffers, a vector of buffers
 // fix so that is an array of triples: buffer, offset length, not just one offset, length. Fixes edge cases in truncate if then add more... which can do now ended removed
-function VBuf() {
-	this.offset = 0;
-	this.length = 0;
-	this.buffers = [];
-}
+// hmm, without init all get same buffer!
 
-VBuf.prototype.data = function(buf) {
-	this.buffers.push(buf);
-	this.length += buf.length;
-};
-	
-VBuf.prototype.eat = function(len) {
-	if (len === 0) {return;}
-	if (len > this.length) {len = this.length;}
-	this.offset += len;
-	this.length -= len;
-	while (this.buffers.length !== 0 && this.offset >= this.buffers[0].length) {
-		this.offset -= this.buffers[0].length;
-		this.buffers.shift();
-	}
-};
-
-VBuf.prototype.truncate = function(len) {
-	// truncate this vbuf
-	if (len > this.length) {len = this.length;}
-	var drop = this.length - len;
-	while (this.buffers[this.buffers.length - 1].length <= drop) {
-		drop -= this.buffers[this.buffers.length - 1].length;
-		this.buffers.pop();
-	}
-	this.length = len;
-};
-
-VBuf.prototype.ref = function(len) {
-	// return a truncated vbuf object, can be used to store a reference to the front of stream
-	var trunc = new VBuf();
-	trunc.buffers = this.buffers.slice();
-	trunc.offset = this.offset;
-	trunc.length = this.length;
-	trunc.truncate(len);
-	return trunc;
-};
-
-VBuf.prototype.bytes = function(len) {
-	var offset = this.offset;
-	var bytes = [];
-	var buf = 0;
-	if (len > this.length) {len = this.length;}
-	for (var i = 0; i < len; i++) {
-		while (this.buffers[buf].length <= offset) {
-			offset = 0;
-			buf++;
+var vbuf = {
+	init: function() {
+		this.buffers = [];
+		this.offset = 0;
+		this.length = 0;
+	},
+	data: function(buf) {
+		this.buffers.push(buf);
+		this.length += buf.length;
+	},
+	eat: function(len) {
+		if (len === 0) {
+			return;	
 		}
-		bytes.push(this.buffers[buf][offset++]);
+		if (len > this.length) {
+			len = this.length;
+			}
+			this.offset += len;
+			this.length -= len;
+			while (this.buffers.length !== 0 && this.offset >= this.buffers[0].length) {
+				this.offset -= this.buffers[0].length;
+				this.buffers.shift();
+			}
+	},
+	truncate: function(len) {
+		// truncate this vbuf
+		if (len > this.length) {len = this.length;}
+		var drop = this.length - len;
+		while (this.buffers[this.buffers.length - 1].length <= drop) {
+			drop -= this.buffers[this.buffers.length - 1].length;
+			this.buffers.pop();
+		}
+		this.length = len;
+	},
+	ref: function(len) {
+		// return a truncated vbuf object, can be used to store a reference to the front of stream
+		var trunc = Object.create(this);
+		trunc.buffers = this.buffers.slice(); // need to clone the buffers
+		trunc.truncate(len);
+		return trunc;
+	},
+	bytes: function(len) {
+		var offset = this.offset;
+		var bytes = [];
+		var buf = 0;
+		if (len > this.length) {len = this.length;}
+		for (var i = 0; i < len; i++) {
+			while (this.buffers[buf].length <= offset) {
+				offset = 0;
+				buf++;
+			}
+			bytes.push(this.buffers[buf][offset++]);
+		}
+		return bytes;
 	}
-	return bytes;
 };
 
 // FSM. receives events and has an emitter for the state functions to use.
@@ -86,7 +88,8 @@ VBuf.prototype.bytes = function(len) {
 // should an fsm be a function, so can use it as a component of another fsm? or just evented composition? or can we use functions to compose?
 // also should listen events be constructors?
 
-var FSM = Object.create(events.EventEmitter);
+emitter = new events.EventEmitter; // need to init to make this work.
+var FSM = Object.create(emitter);
 
 // pass the event (but not emitter) to the function
 FSM.listen = function(emitter, ev) {
@@ -114,6 +117,7 @@ FSM.listen = function(emitter, ev) {
 // apply success and fail values
 
 function match(check, success, fail, again, args) {
+	args = Array.prototype.slice.call(args); // only do if needed!
 	var ret = check.apply(this, args);
 	if (typeof ret == 'undefined') {
 		return again;
@@ -291,7 +295,12 @@ function chunk_data(ev) {
 
 function chunk_crc(bytes) {
 	console.log("crc " + (to32(bytes)) + " vs " + this.crc.crc);
-	return (to32(bytes) === this.crc.crc);
+	if ((to32(bytes) !== this.crc.crc)) {
+		return false;
+	}
+	// now emit a chunk event
+	this.emit('chunk', this.chunk_type, this.chunk_data);
+	return true;
 }
 
 var pfsm = Object.create(FSM);
@@ -305,34 +314,35 @@ pfsm.fail = function() {
 };
 
 pfsm.match_signature = function() {
-	return match.call(this, accept([137, 80, 78, 71, 13, 10, 26, 10]), this.match_chunk_len, this.fail, this.match_signature, Array.prototype.slice.call(arguments));
+	return match.call(this, accept([137, 80, 78, 71, 13, 10, 26, 10]), this.match_chunk_len, this.fail, this.match_signature, arguments);
 };
 
 pfsm.match_chunk_len = function() {
-	return match.call(this, get(4, chunk_len), this.match_chunk_type, this.fail, this.match_chunk_len, Array.prototype.slice.call(arguments));
+	return match.call(this, get(4, chunk_len), this.match_chunk_type, this.fail, this.match_chunk_len, arguments);
 };
 
 pfsm.match_chunk_type = function() {
-	return match.call(this, get(4, chunk_type), this.match_chunk_data, this.fail, this.match_chunk_type, Array.prototype.slice.call(arguments));
+	return match.call(this, get(4, chunk_type), this.match_chunk_data, this.fail, this.match_chunk_type, arguments);
 };
 	
 pfsm.match_chunk_data = function() {
-	return match.call(this, chunk_data, this.match_chunk_crc, this.fail, this.match_chunk_data, Array.prototype.slice.call(arguments));
+	return match.call(this, chunk_data, this.match_chunk_crc, this.fail, this.match_chunk_data, arguments);
 };
 	
 pfsm.match_chunk_crc = function() {
-	return match.call(this, get(4, chunk_crc), this.match_eof, this.fail, this.match_chunk_crc, Array.prototype.slice.call(arguments));
+	return match.call(this, get(4, chunk_crc), this.match_eof, this.fail, this.match_chunk_crc, arguments);
 };
 
 function eof(ev) {
 	if (ev === 'end') {
+		this.emit('end');
 		return true;
 	}
 	return (this.vb.length === 0) ? undefined : false;
 }
 
 pfsm.match_eof = function() {
-	return match.call(this, eof, this.success, this.match_chunk_len, this.match_eof, Array.prototype.slice.call(arguments));
+	return match.call(this, eof, this.success, this.match_chunk_len, this.match_eof, arguments);
 };
 
 pfsm.transition = true;
@@ -341,7 +351,8 @@ pfsm.transition = true;
 
 // remove init fn - just put in starting state and allow clone, as far as is possible anyway
 pfsm.init = function(stream) {
-	this.vb = new VBuf();
+	this.vb = Object.create(vbuf);
+	this.vb.init();
 	var vb = this.vb;
 	this.crc = Object.create(crc32);
 	this.state = pfsm.match_signature;
@@ -385,10 +396,11 @@ cfsm.rep = { // used to filter out ones used before. 1 and ? mean can only appea
 	tEXt: "*",
 	zTXt: "*"
 };
-cfsm.next: { // functions of currently allowed set and already used counts, almost table driven state machine
+cfsm.next = { // functions of currently allowed set and already used counts, almost table driven state machine
 	// note that can return items that can be used once only - another check will remove these
+	// do these need to be functions?
 	IHDR: function(a, u) {
-		return ['tIME', 'zTXt', 'tEXt', 'iTXt', 'pHYs', 'sPLT', 'iCCP', 'sRGB', 'sBIT', 'gAMA', 'cHRM', 'tRNS', 'bKGD', 'IDAT', 'PLTE']);
+		return ['tIME', 'zTXt', 'tEXt', 'iTXt', 'pHYs', 'sPLT', 'iCCP', 'sRGB', 'sBIT', 'gAMA', 'cHRM', 'tRNS', 'bKGD', 'IDAT', 'PLTE'];
 	},
 	PLTE: function(a, u) {
 		return ['tIME', 'zTXt', 'tEXt', 'iTXt', 'tRNS', 'hIST', 'bKGD', 'IDAT'];
@@ -415,12 +427,15 @@ cfsm.next: { // functions of currently allowed set and already used counts, almo
 		"tEXt": "*",
 		"zTXt": "*"
 };
-
+cfsm.states = {
+	//IHDR: function
+};
+cfsm.used = {};
 
 
 (function(exports) {
 	exports.FSM = FSM;
-	exports.VBuf = VBuf;
+	exports.vbuf = vbuf;
 	exports.accept = accept;
 	exports.match = match;
 	exports.pfsm = pfsm;
